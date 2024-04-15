72c8068eb61648f2771e557960a93f9ee87cd99f
josetomassilvaz <josetomassilvaz@gmail.com>
2024-04-15 17:55:39 -0400

    feat: :sparkles: add cat querying methods

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from "express"
 
 const PORT = 3000
+let cats = ["Matilda", "Matías", "Niñita"]
+
 
 const app = express()
 app.use(express.json())
+
+app.get("/cats", (req, res)=>{
+    res.status(200).json({cats})
+})
+
+app.get("/cat/:name", (req, res)=>{
+    let {name} = req.params
+    name = name.toLowerCase()
+    const cat = cats.find(cat => cat.toLowerCase() === name)
+    if(!cat) return res.status(404).json({message: "Cat not found"})
+    res.status(200).json({cat})
+})
+
+app.post("/cat", (req, res)=>{
+    const {name} = req.body
+    if(!name) return res.status(400).json({message: "Name is required"})
+
+    cats.push(name)
+    res.status(201).json({message: "Cat added successfully", cats})
+})
 
 
 app.use(express.static("views"))
